ccf524c815804f8a3a51e4eb6dac5c4668c93dc9
Fernando Crespo <fernando@crespo.in>
2013-08-14 20:49:43 -0300

    Refactor some code and document a little more

--- workflows/source/Search Mac Power Users/mpu_search.py
+++ workflows/source/Search Mac Power Users/mpu_search.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# require bs4, requests, lxml, re, shelve
+# title:    Search Mac Power Users show notes
+# author:   Fernando Xavier de Freitas Crespo
+# email:    fernando@crespo.in
+
+# require:  bs4, requests, lxml, re, shelve
 
 from bs4 import BeautifulSoup
 import requests
@@ -13,7 +17,7 @@ query = "{query}"
 
 item_template='  <item arg="{0}" valid="{3}" autocomplete="{0}"><title><![CDATA[{1}]]></title><subtitle><![CDATA[{2}]]></subtitle><icon>icon.png</icon></item>'
 
-def getEpisodesInPage(page):
+def get_episodes_in_page(page):
     episodes = []
     mpu = 'http://5by5.tv/mpu/page/{0}'
     response = requests.get(mpu.format(page))
@@ -22,39 +26,37 @@ def getEpisodesInPage(page):
         for div in soup.findAll('div'):
             if div.get('class') and div.get('class')[0] == 'episode':
                 title = div.h3.a.text.encode('ascii', 'ignore').strip()
-                episode_number = re.search('.*#([0-9]{,3}).*', title).group(1)
+                episode_number = re.search(r'.*#([0-9]{,3}).*', title).group(1)
                 subtitle = div.p.text.encode('ascii', 'ignore').strip()
                 episodes.append([episode_number, title, subtitle])
     return episodes
 
-def addEpisodeToCache(cache, episode):
+def add_episode_to_cache(cache, episode):
     episode_number = episode[0].zfill(4)
     if not cache.has_key(episode_number):
         cache[episode_number] = episode
         return True
     return False
 
-def cacheAll(cache):
+def cache_all(cache):
     for i in range(1, 20): # Pick last 20 pages to cache all episodes
-        episodesInPageI = getEpisodesInPage(i)
-        for episode in episodesInPageI:
+        episodes_in_page_i = get_episodes_in_page(i)
+        for episode in episodes_in_page_i:
             #print "Caching episode {0}".format(episode[0])
             print item_template.format(episode[0], episode[1], episode[2], 'yes')
-            addEpisodeToCache(cache, episode)
+            add_episode_to_cache(cache, episode)
 
-def listAll():
+def list_all():
     for i in range(1, 20): # Pick last 20 pages to cache all episodes
-        episodesInPageI = getEpisodesInPage(i)
-        for episode in episodesInPageI:
+        episodes_in_page_i = get_episodes_in_page(i)
+        for episode in episodes_in_page_i:
             print item_template.format(episode[0], episode[1], episode[2], 'yes')
 
 def main():
-    # Fazer cache de tudo e depois sempre verificar a primeira página para adicionar itens atuais no cache
-
     cached_items = shelve.open('episodes_cache')
 
     if not cached_items:
-        cacheAll(cached_items)
+        cache_all(cached_items)
         cached_items.close()
         cached_items = shelve.open('episodes_cache')
 
@@ -63,26 +65,23 @@ def main():
     print('<?xml version="1.0" encoding="UTF-8"?>\n<items>')
 
     if cached_items:
-        # Force get lastest 10 episodes to keep up to date
-        episodesInPage0 = getEpisodesInPage(0)
-        for episode in episodesInPage0:
-            addEpisodeToCache(cached_items, episode)
+        # Force get lastest 10 episodes (firs page) to keep up to date
+        episodes_in_page_0 = get_episodes_in_page(0)
+        for episode in episodes_in_page_0:
+            add_episode_to_cache(cached_items, episode)
 
         for item in sorted_cached_items:
-            searchIn = str(cached_items[item][0]).lower() + " " + \
+            search_in = str(cached_items[item][0]).lower() + " " + \
             str(cached_items[item][1]).lower() + " " + \
             str(cached_items[item][2]).lower()
-            if query.lower() in searchIn:
+            if query.lower() in search_in:
                 print item_template.format(cached_items[item][0], cached_items[item][1], cached_items[item][2], 'yes')
     else:
-        #for item in getEpisodesInPage(9):
-        #    print item_template.format(item[0], item[1], item[2], 'yes')
-        #for item in getEpisodesInPage(11):
-        #    print item_template.format(item[0], item[1], item[2], 'yes')
-        print item_template.format(0, "Error", "Could not read cache file", 'no')
+        print(item_template.format(0, "Error", "Could not read cache file", 'no'))
 
     print('</items>')
 
     cached_items.close()
 
-main()
+if __name__ == '__main__':
+    main()
